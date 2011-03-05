2ca2e86fecd1cb69ede2d0d3191584becd9cad87
Alexey Ostapenko <kbakba@kbakba.net>
2011-03-05 18:10:53 +0300

    env variables

--- system/env.zsh
+++ system/env.zsh
@@ -1,1 +1,7 @@
-#export EDITOR='mvim'
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
+export GREP_OPTIONS="--exclude="\*/.svn/\*" --color "
+export EDITOR=vim
+
+[ -x /usr/bin/lesspipe ] && eval "$(lesspipe)"
