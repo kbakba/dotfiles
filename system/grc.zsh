4f124d960ebf74e1e40c21aa8b7d12d7dc7301e1
Alexey Ostapenko <kbakba@kbakba.net>
2011-03-22 12:47:27 +0300

    grc config

--- system/grc.zsh
+++ system/grc.zsh
@@ -1,5 +1,17 @@
 # GRC colorizes nifty unix tools all over the place
-if $(grc &> /dev/null)
+GRC=`which grc`
+if [ "$TERM" != dumb ] && [ -n GRC ]
 then
-  source $BREW_PREFIX/etc/grc.bashrc
+    alias colourify="$GRC -es --colour=auto"
+    alias configure='colourify ./configure'
+    alias diff='colourify diff'
+    alias make='colourify make'
+    alias gcc='colourify gcc'
+    alias g++='colourify g++'
+    alias as='colourify as'
+    alias gas='colourify gas'
+    alias ld='colourify ld'
+    alias netstat='colourify netstat'
+    alias ping='colourify ping'
+    alias traceroute='colourify /usr/sbin/traceroute'
 fi
