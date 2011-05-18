5054b0aab7d53c335f83d3d4c2f4d0a921043594
Aleksey Ostapenko <kbakba@kbakba.net>
2011-05-18 23:49:09 +0400

    lesspipe renamed to lesspipe.sh

--- system/env.zsh
+++ system/env.zsh
@@ -4,5 +4,5 @@ export LC_ALL=en_US.UTF-8
 export GREP_OPTIONS="--exclude="\*/.svn/\*" --color "
 export EDITOR=vim
 
-[ -x /usr/bin/lesspipe ] && eval "$(lesspipe)"
+[ -x /usr/bin/lesspipe.sh ] && eval "$(lesspipe.sh)"
 [ -x "$( which most )" ] && export PAGER=most
