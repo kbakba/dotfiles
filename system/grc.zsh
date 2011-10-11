d60cdc2964715b50f1da71274346abf4e72e2176
Aleksey Ostapenko <kbakba@kbakba.net>
2011-10-11 13:53:04 +0400

    disable grc for makefiles

--- system/grc.zsh
+++ system/grc.zsh
@@ -1,11 +1,11 @@
 # GRC colorizes nifty unix tools all over the place
 GRC=`which grc`
-if [ "$TERM" != dumb ] && [ -n GRC ]
+if [ "$TERM" != dumb ] && [ -f $GRC ]
 then
     alias colourify="$GRC -es --colour=auto"
     alias configure='colourify ./configure'
     alias diff='colourify diff'
-    alias make='colourify make'
+#    alias make='colourify make'
     alias gcc='colourify gcc'
     alias g++='colourify g++'
     alias as='colourify as'
