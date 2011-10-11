881be78b365413ba45e76903c9b4ea419fb68553
Aleksey Ostapenko <kbakba@kbakba.net>
2011-10-11 14:00:58 +0400

    special-dirs = true

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -1,5 +1,9 @@
 # matches case insensitive for lowercase
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
+# enable .. completion
+zstyle ':completion:*' special-dirs true
+
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
+
