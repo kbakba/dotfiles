e8f2198ba47ef835cec49ecdcaad5c29f723047a
Aleksey Ostapenko <kbakba@kbakba.net>
2012-05-17 19:55:41 +0400

    autojump config

--- system/autojump.zsh
+++ system/autojump.zsh
@@ -1,5 +1,7 @@
-if [ -f $(brew_prefix autojump)/etc/autojump.zsh ]; then
-    source $(brew_prefix autojump)/etc/autojump.zsh
+if [ -f /usr/share/autojump/autojump.zsh ]; then
+    source /usr/share/autojump/autojump.zsh
 elif [ -f /etc/profile.d/autojump.zsh ]; then
     source /etc/profile.d/autojump.zsh
+elif [ -f $(brew_prefix autojump)/etc/autojump.zsh ]; then
+    source $(brew_prefix autojump)/etc/autojump.zsh
 fi
