c7e9443b66e009e3f4d492e7669291eb6832b625
Aleksey Ostapenko <kbakba@kbakba.net>
2012-09-20 11:55:44 +0400

    Add local autojump config

--- system/autojump.zsh
+++ system/autojump.zsh
@@ -1,7 +1,9 @@
-if [ -f /usr/share/autojump/autojump.zsh ]; then
+if [ -s /usr/share/autojump/autojump.zsh ]; then
     source /usr/share/autojump/autojump.zsh
-elif [ -f /etc/profile.d/autojump.zsh ]; then
+elif [ -s /etc/profile.d/autojump.zsh ]; then
     source /etc/profile.d/autojump.zsh
-elif [ -f $(brew_prefix autojump)/etc/autojump.zsh ]; then
+elif [ -s $(brew_prefix autojump)/etc/autojump.zsh ]; then
     source $(brew_prefix autojump)/etc/autojump.zsh
+elif [ -s ~/.autojump/etc/profile.d/autojump.zsh ]; then
+    source ~/.autojump/etc/profile.d/autojump.zsh
 fi
