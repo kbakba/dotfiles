06c35289e5b9c13aa1888bee39e9e800f1de9458
Aleksey Ostapenko <kbakba@kbakba.net>
2012-11-21 18:50:32 +0400

    add virtualenvwrapper.sh

---
+++ python/virtualenv.zsh
@@ -0,0 +1,5 @@
+# Use virtualenvwrapper http://pypi.python.org/pypi/virtualenvwrapper
+if [ -f /usr/local/bin/virtualenvwrapper.sh ]; then
+    export WORKON_HOME=$HOME/.virtualenvs
+    source /usr/local/bin/virtualenvwrapper.sh
+fi
