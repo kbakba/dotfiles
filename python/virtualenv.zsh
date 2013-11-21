f1e9897312edbf252d31dbadf4949add98f33b1b
Aleksey Ostapenko <kbakba@kbakba.net>
2013-11-21 19:02:48 +0400

    add virtualenv-sh

--- python/virtualenv.zsh
+++ python/virtualenv.zsh
@@ -3,3 +3,17 @@ if [ -f /usr/local/bin/virtualenvwrapper.sh ]; then
     export WORKON_HOME=$HOME/.virtualenvs
     source /usr/local/bin/virtualenvwrapper.sh
 fi
+
+# https://bitbucket.org/psagers/virtualenv-sh
+if [ -f /usr/local/bin/virtualenvwrapper.sh ]; then
+    # Configure all virtualenv-sh functions for autoloading
+    fpath=(/usr/local/bin/virtualenv-sh $fpath)
+    autoload -w /usr/local/bin/virtualenv-sh
+
+    # Call the main initialization function
+    virtualenv_sh_init
+
+    # init autoworkon
+    autoload -U add-zsh-hook
+    add-zsh-hook chpwd autoworkon
+fi
