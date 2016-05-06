6778885d238ce66db53b32d6f78b8e62f9be9c74
Aleksey Ostapenko <kbakba@yandex-team.ru>
2016-05-06 15:20:13 +0300

    $GOPATH/bin to $PATH

--- go/path.zsh
+++ go/path.zsh
@@ -1,8 +1,8 @@
+export GOPATH=$HOME/golang
+if [ -x "$( which go )" ]; then
+    export PATH=$PATH:$GOPATH/bin
+fi
+
 if [ -d /usr/local/go/bin ]; then
     export PATH=$PATH:/usr/local/go/bin
-    export GOPATH=$HOME/golang
-    if [ ! -d $GOPATH ]; then
-        mkdir -p $GOPATH
-    fi
-    export PATH=$PATH:$GOPATH/bin
 fi
