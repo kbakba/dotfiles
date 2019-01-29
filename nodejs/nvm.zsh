e7d188590f2360661536db772cafe066ae4b43e3
Aleksey Ostapenko <kbakba@yandex-team.ru>
2019-01-29 20:13:19 +0300

    nodejs

--- nodejs/nvm.zsh
+++ nodejs/nvm.zsh
@@ -1,4 +1,33 @@
 if [ -s $(brew_prefix nvm)/nvm.sh ]; then
-    export NVM_DIR=~/.nvm
+    export NVM_DIR=$HOME/.nvm
+    export NODE_VERSIONS=$NVM_DIR/versions/node
+    export NODE_VERSION_PREFIX=v
     source $(brew_prefix nvm)/nvm.sh
+
+    # from https://gist.github.com/fl0w/07ce79bd44788f647deab307c94d6922
+    #
+    # lazyload nvm
+    # all props goes to http://broken-by.me/lazy-load-nvm/
+    # grabbed from reddit @ https://www.reddit.com/r/node/comments/4tg5jg/lazy_load_nvm_for_faster_shell_start/
+
+    # lazynvm() {
+    #     unset -f nvm node npm
+    #     source $(brew_prefix nvm)/nvm.sh # This loads nvm
+    # }
+
+    # nvm() {
+    #     lazynvm
+    #     nvm $@
+    # }
+
+    # node() {
+    #     lazynvm
+    #     node $@
+    # }
+
+    # npm() {
+    #     lazynvm
+    #     npm $@
+    # }
+
 fi
--- nodejs/path.zsh
+++ nodejs/path.zsh
@@ -1,9 +1,7 @@
-NPM=`which npm`
-if [ -e $NPM ]; then
+if exists npm ; then
     cd ~
     NPM_BIN=`npm bin`
     NODE_PATH_NPM=`npm root -g`
     export PATH="$NPM_BIN:$PATH"
     export NODE_PATH="$NODE_PATH_NPM:$NODE_PATH"
-
 fi
