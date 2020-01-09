6f39a70f92c138b72cb07dda13aee90ab492493e
Aleksey Ostapenko <github@kbakba.net>
2020-01-09 19:57:11 +0300

    Move from peco to skim

---
+++ system/skim.zsh
@@ -0,0 +1,23 @@
+if exists sk; then
+    # History
+    function skim_select_history() {
+        BUFFER=$(fc -l -n 1 | sk --tac --query "$LBUFFER")
+        CURSOR=$#BUFFER         # move cursor
+        zle -R -c               # refresh
+    }
+
+    zle -N skim_select_history
+    bindkey '^R' skim_select_history
+
+    # Git
+    local GIT_SK="sk --ansi --cycle --height 45%"
+    git config --global --replace-all alias.br "!git branch | fgrep -v '* ' | $GIT_SK | xargs git checkout"
+    git config --global --replace-all alias.bra "!git branch --remotes | sed -e 's#origin/##' | $GIT_SK | xargs git checkout"
+
+    # Pazi intergation
+    if exists z; then
+        unalias zz
+        alias zz="z --pipe 'sk --height 45%'"
+    fi
+fi
+
