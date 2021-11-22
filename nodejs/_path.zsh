ff29ce984ca11605bf5e86c62c08ea0d4ec2ec7d
Aleksey Ostapenko <github@kbakba.net>
2021-11-22 14:05:59 +0300

    Add node_modules/.bin from HOME

--- nodejs/_path.zsh
+++ nodejs/_path.zsh
@@ -5,4 +5,8 @@ if exists npm ; then
     NODE_PATH_NPM=`npm root -g`
     export PATH="$NPM_BIN:$PATH"
     export NODE_PATH="$NODE_PATH_NPM:$NODE_PATH"
+
+    if [ -d $HOME/node_modules/.bin ]; then
+        export PATH="$HOME/node_modules/.bin:$PATH"
+    fi
 fi
